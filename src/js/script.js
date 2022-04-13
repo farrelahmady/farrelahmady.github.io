a20fe7d20f2e594602b0b1d40c8bc4e0034f1adc
farrelfay21 <farrelfay.251@gmail.com>
2022-04-14 01:31:38 +0700

    portfolio with swiperjs

--- src/js/script.js
+++ src/js/script.js
@@ -18,3 +18,29 @@ hamburger.addEventListener("click", () => {
 	hamburger.classList.toggle("hamburger-active");
 	navMenu.classList.toggle("hidden");
 });
+
+// Swiper
+
+var swiper = new Swiper("#portfolio-swiper", {
+	slidesPerView: 1,
+	slidesPerGroup: 1,
+	keyboard: {
+		enabled: true,
+	},
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	breakpoints: {
+		768: {
+			slidesPerView: 2,
+		},
+		1024: {
+			slidesPerView: 3,
+		},
+	},
+	navigation: {
+		nextEl: "#next",
+		prevEl: "#prev",
+	},
+});
